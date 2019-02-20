1393400f76f669e714d53f5a29a21b24a0502bd9
Nate Higgins <nat@nath.is>
2019-02-20 01:19:05 +0000

    fix bug collecting tweets

--- tw-delete.js
+++ tw-delete.js
@@ -1,9 +1,9 @@
 const Twitter = require("twitter");
-const { promisify } = require('util');
-const readline = require('readline');
-const fs = require('fs');
-const path = require('path');
-const PromisePool = require('es6-promise-pool');
+const { promisify } = require("util");
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
+const PromisePool = require("es6-promise-pool");
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
@@ -13,142 +13,186 @@ const {
   TWD_CONSUMER_SECRET,
   TWD_ACCESS_TOKEN,
   TWD_ACCESS_TOKEN_SECRET,
-  TWD_CACHE_FILE = path.resolve('./cache.json')
+  TWD_CACHE_FILE = path.resolve("./cache.json")
 } = process.env;
 
-if (!TWD_CONSUMER_KEY || !TWD_CONSUMER_SECRET || !TWD_ACCESS_TOKEN || !TWD_ACCESS_TOKEN_SECRET) {
-    throw new Error('Invalid config');
+if (
+  !TWD_CONSUMER_KEY ||
+  !TWD_CONSUMER_SECRET ||
+  !TWD_ACCESS_TOKEN ||
+  !TWD_ACCESS_TOKEN_SECRET
+) {
+  throw new Error("Invalid config");
 }
 
 function sleep(ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
 
-async function findLastTweetOnDate(get, endDate, maxId, delay = 50, attempt = 1) {
-    console.log(`Attempt ${attempt}`);
-    const tweets = await get('statuses/user_timeline', { max_id: maxId, count: 200 });
+async function findLastTweetOnDate(
+  get,
+  endDate,
+  maxId,
+  delay = 50,
+  attempt = 1
+) {
+  console.log(`Attempt ${attempt}`);
+  const tweets = await get("statuses/user_timeline", {
+    max_id: maxId,
+    count: 200
+  });
 
-    for (const tweet of tweets) {
-        if ((new Date(tweet.created_at)) < endDate) {
-            return tweet;
-        }
+  for (const tweet of tweets) {
+    if (new Date(tweet.created_at) < endDate) {
+      return tweet;
     }
+  }
 
-    if (attempt >= 5) {
-        throw new Error(`Could not find last tweet on ${endDate}`);
-    }
+  if (attempt >= 5) {
+    throw new Error(`Could not find last tweet on ${endDate}`);
+  }
 
-    await sleep(delay);
+  await sleep(delay);
 
-    const lastId = tweets[tweets.length - 1].id_str;
-    return await findLastTweetOnDate(get, endDate, maxId, delay + 50, attempt + 1);
+  const lastId = tweets[tweets.length - 1].id_str;
+  return await findLastTweetOnDate(
+    get,
+    endDate,
+    maxId,
+    delay + 50,
+    attempt + 1
+  );
 }
 
 function prompt(prompt) {
-    return new Promise(resolve => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-            terminal: false
-        });
+  return new Promise(resolve => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
 
-        rl.question(prompt, answer => {
-            rl.close();
-            resolve(answer);
-        });
-    })
+    rl.question(prompt, answer => {
+      rl.close();
+      resolve(answer);
+    });
+  });
 }
 
 async function collectTweets(get, maxId, collectedTweets) {
-    if (!collectedTweets) {
-        try {
-            collectedTweets = JSON.parse(await readFile(TWD_CACHE_FILE));
-        } catch (e) {
-            if (!e.code === 'ENOENT') {
-                throw e;
-            }
+  if (!collectedTweets) {
+    try {
+      collectedTweets = JSON.parse(await readFile(TWD_CACHE_FILE));
+    } catch (e) {
+      if (!e.code === "ENOENT") {
+        throw e;
+      }
 
-            collectedTweets = [];
-        }
+      collectedTweets = [];
     }
+  }
 
-    if (collectedTweets.length) {
-        console.log(maxId, collectedTweets[collectedTweets.length - 1].id_str);
-        maxId = collectedTweets[collectedTweets.length - 1].id_str;
-    }
+  if (collectedTweets.length) {
+    maxId = collectedTweets[collectedTweets.length - 1].id_str;
+  }
 
-    console.log('Collecting…', collectedTweets.length);
+  console.log("Collecting…", collectedTweets.length);
 
-    const newTweets = await get('statuses/user_timeline', { max_id: maxId, count: 200 });
-    const filteredTweets = newTweets.filter(nTweet => !collectedTweets.find(tw => nTweet.id_str !== tw.id_str));
+  const newTweets = await get("statuses/user_timeline", {
+    max_id: maxId,
+    count: 200
+  });
+  const filteredTweets = newTweets.filter(
+    nTweet => !collectedTweets.find(tw => nTweet.id_str === tw.id_str)
+  );
 
-    if (!filteredTweets.length) {
-        return collectedTweets;
-    }
+  if (!filteredTweets.length) {
+    return collectedTweets;
+  }
 
-    console.log(`Found ${filteredTweets.length} tweets`);
+  console.log(`Found ${filteredTweets.length} tweets`);
 
-    const tweets = [...collectedTweets, ...filteredTweets];
-    await writeFile(TWD_CACHE_FILE, JSON.stringify(tweets));
+  const tweets = [...collectedTweets, ...filteredTweets];
+  await writeFile(TWD_CACHE_FILE, JSON.stringify(tweets));
 
-    await sleep(1000);
+  await sleep(1000);
 
-    return await collectTweets(get, maxId, tweets);
+  return await collectTweets(get, maxId, tweets);
 }
 
-async function deleteTweet(post, tweet) {
-
+async function deleteTweet(post, tweet, retry = 0) {
+  try {
     await post(`statuses/destroy/${tweet.id_str}`);
+  } catch (e) {
+    if (retry < 3) {
+      return await deleteTweet(post, tweet, retry + 1);
+    }
 
-    await sleep(100);
+    // Ignore tweet already deleted
+    if (!e || e.length !== 1 || e[0].code !== 144) {
+        throw e;
+    }
+  }
+
+  await sleep(100);
 }
 
 async function deleteTweets(post, tweets) {
-    function* generator() {
-        for (const [count, tweet] of Object.entries(tweets)) {
-            console.log(`Deleting ${count}/${tweets.length}`);
-            yield deleteTweet(post, tweet);
-        }
+  function* generator() {
+    let newTweets = tweets;
+
+
+    for (const [count, tweet] of Object.entries(tweets)) {
+      console.log(`Deleting ${count}/${tweets.length}`);
+      yield deleteTweet(post, tweet);
+
+      newTweets = tweets.filter(tw => tw.id_str !== tweet);
+      yield writeFile('./cache.json', JSON.stringify(newTweets));
     }
+  }
 
-    const pool = new PromisePool(generator, 5);
+  const pool = new PromisePool(generator, 5);
 
-    await pool.start();
+  await pool.start();
 }
 
-async function main([ endDateString ]) {
-    if (!`${endDateString}`.match(/^\d{4}-\d{2}-\d{2}$/)) {
-        console.error('Usage: ./tw-delete YYYY-MM-DD');
-        return process.exit(1);
-    }
+async function main([endDateString]) {
+  if (!`${endDateString}`.match(/^\d{4}-\d{2}-\d{2}$/)) {
+    console.error("Usage: ./tw-delete YYYY-MM-DD");
+    return process.exit(1);
+  }
 
-    const endDate = new Date(`${endDateString}T23:59:59.999Z`);
+  const endDate = new Date(`${endDateString}T23:59:59.999Z`);
 
-    const client = new Twitter({
-        consumer_key: TWD_CONSUMER_KEY,
-        consumer_secret: TWD_CONSUMER_SECRET,
-        access_token_key: TWD_ACCESS_TOKEN,
-        access_token_secret: TWD_ACCESS_TOKEN_SECRET,
-    });
+  const client = new Twitter({
+    consumer_key: TWD_CONSUMER_KEY,
+    consumer_secret: TWD_CONSUMER_SECRET,
+    access_token_key: TWD_ACCESS_TOKEN,
+    access_token_secret: TWD_ACCESS_TOKEN_SECRET
+  });
 
-    const get = promisify(client.get.bind(client));
-    const post = promisify(client.post.bind(client));
+  const get = promisify(client.get.bind(client));
+  const post = promisify(client.post.bind(client));
 
-    console.log('Finding last tweet');
+  console.log("Finding last tweet");
 
-    const lastTweet = await findLastTweetOnDate(get, endDate);
-    const promptResult = await prompt(`Is this the last tweet you would like to delete?\n${lastTweet.text}: [Y/N] `);
+  const lastTweet = await findLastTweetOnDate(get, endDate);
+  const promptResult = await prompt(
+    `Is this the last tweet you would like to delete?\n${
+      lastTweet.text
+    }: [Y/N] `
+  );
 
-    if (!promptResult.match(/^\s*[Yy]\s*/)) {
-        throw new Error('Could not find last tweet');
-    }
-    
-    const tweets = await collectTweets(get, lastTweet.id_str);
+  if (!promptResult.match(/^\s*[Yy]\s*/)) {
+    throw new Error("Could not find last tweet");
+  }
 
-    await deleteTweets(post, tweets);
-    await writeFile('./cache.json', '[]');
+  const tweets = await collectTweets(get, lastTweet.id_str);
+
+  await deleteTweets(post, tweets);
+  await writeFile("./cache.json", "[]");
 }
 
 main(process.argv.slice(2)).then(null, console.error);
